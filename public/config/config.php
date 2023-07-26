270b5cd3442df04d25d8631b92bc9252c666852c
elmarce23 <andresmarcelo55@gmail.com>
2023-07-26 17:07:42 -0600

    Initial Git

---
+++ public/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "datacases");
+define("DB_USER", "marce");
+define("DB_PASS", "");
+
+
+/* Default options */
+define("DEFAULT_CONTROLLER", "note");
+define("DEFAULT_ACTION", "list");
+define('CURRENT_DATE', date("Y-m-d"));
+
+
+?>

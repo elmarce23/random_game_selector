02e66ccd30acc14efa8e424f9750be3c2d4b4dc0
elmarce23 <andresmarcelo55@gmail.com>
2023-07-26 17:35:44 -0600

    Añadiendo Views

---
+++ public/controllers/platfomrs.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once 'models/platforms.php';
+
+class platfController{
+	public $page_title;
+	public $view;
+    public $platfObj;
+
+	public function __construct() {
+		$this->view = 'list_platf';
+		$this->page_title = '';
+		$this->platfObj = new Platform();
+	}
+
+	/* List all notes */
+	public function list(){
+		$this->page_title = 'Listado de notas';
+		return $this->platfObj->getAll();
+	}
+
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,29 +1,49 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-require_once 'config/config.php';
-require_once 'model/db.php';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-if(!isset($_GET["controller"])) $_GET["controller"] = constant("DEFAULT_CONTROLLER");
-if(!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
+    <title>Random Game Selector</title>
+</head>
 
-$controller_path = 'controller/'.$_GET["controller"].'.php';
+<body>
 
-/* Check if controller exists */
-if(!file_exists($controller_path)) $controller_path = 'controller/'.constant("DEFAULT_CONTROLLER").'.php';
+    <?php
 
-/* Load controller */
-require_once $controller_path;
-$controllerName = $_GET["controller"].'Controller';
-$controller = new $controllerName();
+    require_once 'config/config.php';
+    require_once 'model/db.php';
 
-/* Check if method is defined */
-$dataToView["data"] = array();
-if(method_exists($controller,$_GET["action"])) $dataToView["data"] = $controller->{$_GET["action"]}();
+    if (!isset($_GET["controller"])) $_GET["controller"] = constant("DEFAULT_CONTROLLER");
+    if (!isset($_GET["action"])) $_GET["action"] = constant("DEFAULT_ACTION");
+
+    $controller_path = 'controller/' . $_GET["controller"] . '.php';
+
+    /* Check if controller exists */
+    if (!file_exists($controller_path)) $controller_path = 'controller/' . constant("DEFAULT_CONTROLLER") . '.php';
+
+    /* Load controller */
+    require_once $controller_path;
+    $controllerName = $_GET["controller"] . 'Controller';
+    $controller = new $controllerName();
+
+    /* Check if method is defined */
+    $dataToView["data"] = array();
+    if (method_exists($controller, $_GET["action"])) $dataToView["data"] = $controller->{$_GET["action"]}();
 
 
-/* Load views */
-//require_once 'view/template/header.php';
-require_once 'view/'.$controller->view.'.php';
-require_once 'view/footer.php';
+    /* Load views */
+    //require_once 'view/template/header.php';
+    require_once 'views/' . $controller->view . '.php';
+    require_once 'views/footer.php';
 
-?>
+    ?>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
